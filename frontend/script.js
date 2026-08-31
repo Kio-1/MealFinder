08d7bb38d2bb0283c38529f291cfbc7a3c6d50ef
bilal <bilaladilk@gmail.com>
2026-08-31 14:35:58 +0530

    updated js url

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://127.0.0.1:5000/api";
+const API_BASE_URL = "https://mealfinder-fi9a.onrender.com/api";
 let currentUser = null;
 let activeTags = [];
 let weightChartInstance = null; 
